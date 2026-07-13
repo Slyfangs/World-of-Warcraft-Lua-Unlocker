ba247e25f87127bb3565f38305b52d82cda0a0ea
Daddychilll <Slyfangsbusines@proton.me>
2026-07-13 20:15:57 +0300

    Update: Use correct 3.3.5a pattern bytes from binary analysis

--- Lua Unlocker.cpp
+++ Lua Unlocker.cpp
@@ -8,6 +8,8 @@
 
 // 3.3.5a Pattern: Bypasses the insecure function check for protected Lua functions
 // like CastSpellByName and CastSpellByID
+// Pattern: 56 8B F1 8B 0D ? ? ? ? 8B 11 FF 92 ? ? ? ? 84 C0 74 10
+// Mask:    xxxxx????xxxx????xxxx
 const unsigned char NewBytes[] = { 0xEB, 0x10 };
 const unsigned char PatternBytes[] = { 0x56, 0x8B, 0xF1, 0x8B, 0x0D, 0x00, 0x00, 0x00, 0x00, 0x8B, 0x11, 0xFF, 0x92, 0x00, 0x00, 0x00, 0x00, 0x84, 0xC0, 0x74, 0x10 };
 const char PatternMask[] = "xxxxx????xxxx????xxxx";
@@ -50,6 +52,7 @@ int __stdcall DllMain(void* Module, unsigned long Reason, void*)
 	}
 
 	// Offset to the conditional jump (74 10) that needs to be patched
+	// Pattern is 21 bytes long (0-20), and "74 10" is at the end (bytes 19-20)
 	PatchAddress += 0x13;
 
 	unsigned long OldProtection = 0;
